07ecfc47e44ef09c4639f852a57705529a2cd652
tuozhaobing <1044176601@qq.com>
2019-01-03 17:03:54 +0800

    [app module:]ARouter path init

--- app/src/main/java/com/example/administrator/modularsarchitecturedemo/MainActivity.kt
+++ app/src/main/java/com/example/administrator/modularsarchitecturedemo/MainActivity.kt
@@ -3,12 +3,20 @@ package com.example.administrator.modularsarchitecturedemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.alibaba.android.arouter.launcher.ARouter
+import com.example.commonsdk.RouterHub
+import kotlinx.android.synthetic.main.activity_main.*
 
+@Route(path = RouterHub.APP_MAIN_ACTIVITY)
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
+        weather.setOnClickListener {
+            ARouter.getInstance().build(RouterHub.WEATHER_MAIN_ACTIVITY).navigation(this@MainActivity)
+        }
     }
 }
