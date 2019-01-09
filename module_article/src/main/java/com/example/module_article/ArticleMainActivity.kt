691b7e1a22024a565351ff17e691daf533277b42
tuozhaobing <1044176601@qq.com>
2019-01-09 16:39:24 +0800

    [module article]ArticleMainActivity layout and data input completed

--- module_article/src/main/java/com/example/module_article/ArticleMainActivity.kt
+++ module_article/src/main/java/com/example/module_article/ArticleMainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.module_article
 
-import android.util.Log
+import android.text.Html
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.databinding.Observable
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.example.commonsdk.RouterHub
 import com.example.commonsdk.base.BaseActivity
 import com.example.commonservice.article.ResponseArticleData
+import kotlinx.android.synthetic.main.article_activity_main.*
 
 @Route(path = RouterHub.ARTICLE_MAIN_ACTIVITY)
 class ArticleMainActivity : BaseActivity() {
 
     private lateinit var articleMainViewModel: ArticleMainViewModel
+
+    private lateinit var drawerToggle:ActionBarDrawerToggle
 
     override fun getLayout(): Int = R.layout.article_activity_main
 
@@ -21,12 +25,30 @@ class ArticleMainActivity : BaseActivity() {
             todayArticle.addOnPropertyChangedCallback(object :Observable.OnPropertyChangedCallback(){
                 override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
                     var todayArticle:ResponseArticleData = todayArticle.get() as ResponseArticleData
-                    Log.e("TZB",todayArticle.toString())
+                    article_author.text = todayArticle.author
+                    article_content.text = Html.fromHtml(todayArticle.content)
+                    article_title.text = todayArticle.title
                 }
             })
         }
 
         articleMainViewModel.loadTodayArticle()
+
+        drawerToggle = ActionBarDrawerToggle(this,article_main_drawer_layout,R.string.article_drawer_open,R.string.article_drawer_close)
+        article_main_drawer_layout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        article_open_left_drawer_iv.setOnClickListener {
+            if (!article_main_drawer_layout.isDrawerOpen(article_main_drawer_left_layout)){
+                article_main_drawer_layout.openDrawer(article_main_drawer_left_layout)
+            }
+        }
+
+        article_open_right_drawer_iv.setOnClickListener {
+            if (!article_main_drawer_layout.isDrawerOpen(article_main_drawer_right_layout)){
+                article_main_drawer_layout.openDrawer(article_main_drawer_right_layout)
+            }
+        }
     }
 
     override fun initComponent() {
